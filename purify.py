9ee88cab01f330ebae4d902c757fbb74b7b8e21e
dhleeone <78197111+dhleeone@users.noreply.github.com>
2021-11-24 17:18:02 +0900

    Update purify.py

--- purify.py
+++ purify.py
@@ -232,26 +232,4 @@ def purifier(string):
         return mes_li
     else:
         return ""
-                
-
-# def purifier(string):
-#     key_li = []
-#     val_li = []
-#     for i in range(len(keys)):
-#         cnt=0
-#         for j in range(len(values[i])):
-#             if values[i][j] in string:
-#                 key_li.append(keys[i])
-#                 val_li.append(values[i][j])
-#                 cnt+=1
-#             if cnt>=1:
-#                 break;
-                
-#     if len(key_li)== 1:            
-#         purified_mes = f"추천 표현: '{val_li[0]}'(을)를 '{key_li[0]}'(으)로 바꾸어 표현해주세요!"
-#         return purified_mes
-#     elif len(key_li)>1:  
-#         purified_mes = f"추천 표현: {val_li[:]}(을)를 {key_li[:]}(으)로 바꾸어 표현해주세요!"
-#         return purified_mes        
-#     else:
-#         return ""
+            
