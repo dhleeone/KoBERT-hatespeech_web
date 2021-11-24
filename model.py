26e1e9cc120ed17a3eb54262085180517b7d9a8e
dhleeone <78197111+dhleeone@users.noreply.github.com>
2021-11-24 17:25:48 +0900

    Update model.py

--- model.py
+++ model.py
@@ -19,36 +19,14 @@ from transformers.optimization import get_cosine_schedule_with_warmup
 
 bertmodel, vocab = get_pytorch_kobert_model()
 
-
-import pandas as pd
-import xlrd
-
-
-chatbot_data = pd.read_excel('dataset.xlsx', engine='openpyxl' )
-
-
-chatbot_data.loc[(chatbot_data['label'] == "0"), 'label'] = 0  #중립 => 0
-chatbot_data.loc[(chatbot_data['label'] == "1"), 'label'] = 1  #젠더,퀴어 =>1
-chatbot_data.loc[(chatbot_data['label'] == "2"), 'label'] = 2  #지역 => 2
-chatbot_data.loc[(chatbot_data['label'] == "3"), 'label'] = 3  #인종 => 3
-chatbot_data.loc[(chatbot_data['label'] == "4"), 'label'] = 4  #정치 => 4
-chatbot_data.loc[(chatbot_data['label'] == "5"), 'label'] = 5  #세대 => 5
-chatbot_data.loc[(chatbot_data['label'] == "6"), 'label'] = 6  #종교 => 6
-
-data_list = []
-for q, label in zip(chatbot_data['contents'], chatbot_data['label'])  :
-    data = []
-    data.append(q)
-    data.append(str(label))
-
-    data_list.append(data)
-    
-    
-    
-#train & test 데이터로 나누기
-from sklearn.model_selection import train_test_split
-                                                         
-dataset_train, dataset_test = train_test_split(data_list, test_size=0.25, random_state=0)
+# Setting parameters
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 10
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
 
 class BERTDataset(Dataset):
     def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
@@ -66,27 +44,6 @@ class BERTDataset(Dataset):
         return (len(self.labels))
     
     
-# Setting parameters
-max_len = 64
-batch_size = 64
-warmup_ratio = 0.1
-num_epochs = 10
-max_grad_norm = 1
-log_interval = 200
-learning_rate =  5e-5
-
-
-#토큰화
-tokenizer = get_tokenizer()
-tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
-
-data_train = BERTDataset(dataset_train, 0, 1, tok, max_len, True, False)
-data_test = BERTDataset(dataset_test, 0, 1, tok, max_len, True, False)
-
-train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, num_workers=2)
-test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=batch_size, num_workers=2)
-
-
 class BERTClassifier(nn.Module):
     def __init__(self,
                  bert,
@@ -116,35 +73,10 @@ class BERTClassifier(nn.Module):
             out = self.dropout(pooler)
         return self.classifier(out)
     
-    
 #BERT 모델 불러오기
-model = BERTClassifier(bertmodel,  dr_rate=0.5)
-
-#optimizer와 schedule 설정
-no_decay = ['bias', 'LayerNorm.weight']
-optimizer_grouped_parameters = [
-    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-]
-
-optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
-loss_fn = nn.CrossEntropyLoss()
-
-t_total = len(train_dataloader) * num_epochs
-warmup_step = int(t_total * warmup_ratio)
-
-scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
-
-#정확도 측정을 위한 함수 정의
-def calc_accuracy(X,Y):
-    max_vals, max_indices = torch.max(X, 1)
-    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
-    return train_acc
-    
-train_dataloader
+model = BERTClassifier(bertmodel,  dr_rate=0.5) 
 
 
-#모델로드
 device = torch.device('cpu') 
 path = "model.pth" 
 #model = TheModelClass(*args, **kwargs) 
@@ -155,7 +87,6 @@ model.load_state_dict(torch.load(path, map_location=device) , strict=False)
 #토큰화
 tokenizer = get_tokenizer()
 tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
-   
 
 def mespredict(predict_sentence):
     
@@ -203,3 +134,5 @@ def mespredict(predict_sentence):
 
             elif np.argmax(logits) == 0: #중립
               return " "
+            
+        
